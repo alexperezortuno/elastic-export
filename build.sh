aa4d12540ffafbfacdeadc144f0bd9c770f801c8
alexperezortuno <alexperezortuno@gmail.com>
2022-04-20 21:54:14 -0400

    fix pyinstaller builder

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 ver=$(grep '^VERSION_ID' /etc/os-release | awk -F'=' ' gsub(/"/,"") { print $2}')
 
-source bin/activate
 pyinstaller main.py -F  -n "pylastic-ubuntu-${ver}-x86_x64" \
---onefile \
---noconfirm \
---hidden-import elasticsearch \
---hidden-import elasticsearch_dsl \
---hidden-import pandas \
+--onefile --noconfirm \
+--log-level=INFO \
+--add-data=README.md:. \
+--hidden-import=elasticsearch \
+--hidden-import=elasticsearch_dsl \
+--hidden-import=pandas \
+--hidden-import=numpy \
+--hidden-import=coloredlogs \
 --clean
