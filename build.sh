91da4f8b1e3dbea0b7133f6cc70531bb4f4a6d2e
alexperezortuno <alexperezortuno@gmail.com>
2022-04-21 09:51:58 -0400

    change builder

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 ver=$(grep '^VERSION_ID' /etc/os-release | awk -F'=' ' gsub(/"/,"") { print $2}')
+name=$(grep '^NAME' /etc/os-release | awk -F'=' ' gsub(/"/,"") { print $2}')
+name=${name//" "/"-"}
+name=`echo "$name" | awk '{ print tolower($1) }'`
 
-pyinstaller main.py -F  -n "pylastic-ubuntu-${ver}-x86_x64" \
+pyinstaller main.py -F  -n "pylastic-${name}-${ver}" \
 --onefile --noconfirm \
 --log-level=INFO \
 --add-data=README.md:. \
