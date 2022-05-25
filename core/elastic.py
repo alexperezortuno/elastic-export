575fa99c56485146ac2a306e8e82c4fc5ebfc45d
alexperezortuno <alexperezortuno@gmail.com>
2022-05-25 19:40:14 -0400

    add type elastic params and convert to class

--- core/elastic.py
+++ core/elastic.py
@@ -1,3 +1,5 @@
+import os
+
 import pandas as pd
 import json
 from urllib.parse import unquote
@@ -6,6 +8,60 @@ from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search, Q
 from typing import List, Optional
 from core.log import logger
+from core.types.elasticParams import ElasticParams
+
+UNITS_MAPPING = [
+    (1 << 50, ' PB'),
+    (1 << 40, ' TB'),
+    (1 << 30, ' GB'),
+    (1 << 20, ' MB'),
+    (1 << 10, ' KB'),
+    (1, (' byte', ' bytes')),
+]
+
+
+def get_size(filepath: str, unit: str) -> float:
+    size = os.path.getsize(filepath)
+    if unit == 'b':
+        return size
+    elif unit == 'kb':
+        return size / float(1 << 7)
+    elif unit == 'KB':
+        return size / float(1 << 10)
+    elif unit == 'mb':
+        return size / float(1 << 17)
+    elif unit == 'MB':
+        return size / float(1 << 20)
+    elif unit == 'gb':
+        return size / float(1 << 27)
+    elif unit == 'GB':
+        return size / float(1 << 30)
+    elif unit == 'tb':
+        return size / float(1 << 37)
+    elif unit == 'TB':
+        return size / float(1 << 40)
+
+
+def pretty_size(bytesParm, units=None) -> str:
+    """Get human-readable file sizes.
+    simplified version of https://pypi.python.org/pypi/hurry.filesize/
+    """
+    global suffix, factor
+
+    if units is None:
+        units = UNITS_MAPPING
+    for factor, suffix in units:
+        if bytesParm >= factor:
+            break
+    amount = int(bytesParm / factor)
+
+    if isinstance(suffix, tuple):
+        singular, multiple = suffix
+        if amount == 1:
+            suffix = singular
+        else:
+            suffix = multiple
+    return str(amount) + suffix
 
 
 def connect(hosts) -> Elasticsearch:
@@ -36,38 +92,18 @@ def info(es: Elasticsearch):
 
 def search(es: Elasticsearch,
            index: str,
-           gte: str = None,
-           lte: str = None,
-           size: int = 10,
-           query: str = '',
-           query_string: str = '',
-           scroll: str = '1m',
-           scroll_id: str = None,
-           file_name: str = None,
-           headers: bool = False,
-           sort: List[str] = None,
-           showquery: bool = False,
-           rawquery: str = '') -> Optional[pd.DataFrame]:
+           parameters: ElasticParams) -> Optional[pd.DataFrame]:
     """
     Search for documents in Elasticsearch.
-    :param es: string
+    :param es: Elasticsearch
     :param index: string
-    :param gte: string
-    :param lte: string
-    :param size: int
-    :param query: string
-    :param query_string: string
-    :param scroll: string
-    :param scroll_id: string
-    :param file_name: string
-    :param headers: bool
-    :param sort: List[str]
-    :param showquery: bool
+    :param parameters: ElasticParams
     :return:
     """
+    # print(f"Searching for {parameters.gte}")
     try:
-        date_gte = datetime.strptime(gte, "%Y-%m-%dT%H:%M:%S") if gte else None
-        date_lte = datetime.strptime(lte, "%Y-%m-%dT%H:%M:%S") if lte else None
+        date_gte = datetime.strptime(parameters.gte, "%Y-%m-%dT%H:%M:%S") if parameters.gte else None
+        date_lte = datetime.strptime(parameters.lte, "%Y-%m-%dT%H:%M:%S") if parameters.lte else None
         must = []
 
         f = Q('range',
@@ -82,57 +118,60 @@ def search(es: Elasticsearch,
         q = Q('bool', must=must, filter=[f])
 
         body: dict = {
-            'size': size,
+            'size': parameters.size,
             'query': q.to_dict(),
         }
 
-        if query_string != '':
-            body.get('query').get('bool')['must'] = [{'query_string': {'query': query_string}}]
+        if parameters.query_string != '':
+            body.get('query').get('bool')['must'] = [{'query_string': {'query': parameters.query_string}}]
 
-        if query != '':
-            q = query.split(',')
+        if parameters.query != '':
+            q = parameters.query.split(',')
             for i in q:
                 must_string = i.split('=')
                 # body.get('query').get('bool')['must'].append(Q('match', **{must_string[0]: must_string[1]}))
 
-        if rawquery != '':
-            body['query'] = json.loads(unquote(rawquery))
+        if parameters.rawquery != '':
+            body['query'] = json.loads(unquote(parameters.rawquery))
 
-        if showquery:
+        if parameters.showquery:
             print(json.dumps(body, indent=4))
             # with open(file_name, 'w', encoding='utf-8') as f:
             #     json.dump(body, f, ensure_ascii=False, indent=4)
             return None
 
-        if scroll_id is None:
-            response = es.search(index=index, body=body, size=size, sort=sort, scroll=scroll)
+        if parameters.scroll_id is None:
+            response = es.search(index=index,
+                                 body=body,
+                                 size=parameters.size,
+                                 sort=parameters.sort,
+                                 scroll=parameters.scroll)
         else:
-            response = es.scroll(scroll=scroll, scroll_id=scroll_id)
+            response = es.scroll(scroll=parameters.scroll, scroll_id=parameters.scroll_id)
 
         if len(response.get('hits').get('hits')) > 0:
+            if parameters.max_size is not None and parameters.max_size <= int(get_size(parameters.file, 'MB')):
+                return None
+
             l: List = response.get('hits').get('hits')
             data_list: List = []
 
             for i in l:
                 data_list.append(i['_source'].values())
 
+            # file = open(parameters.file)
+            # file.seek(0, os.SEEK_END)
+
+            # print(f"{pretty_size(file.tell())}")
+
             data = pd.DataFrame(data_list, columns=i['_source'].keys())
-            data.to_csv(file_name, mode='a', header=headers, index=False)
+            data.to_csv(parameters.file, mode='a', header=parameters.headers, index=False)
+            parameters.count += len(data_list)
+            parameters.scroll_id = response.get('_scroll_id')
 
             return search(es,
                           index,
-                          gte,
-                          lte,
-                          size,
-                          query,
-                          query_string,
-                          scroll,
-                          response.get('_scroll_id'),
-                          file_name,
-                          False,
-                          sort,
-                          showquery,
-                          rawquery)
+                          parameters)
 
         return response
     except Exception as e:
--- main.py
+++ main.py
@@ -4,32 +4,32 @@ from elasticsearch import Elasticsearch
 from core.log import logger
 from core.elastic import connect, info, search
 from core.tools import create_if_not_exist
+from core.types.elasticParams import ElasticParams
 from globals import documentation
 from settings import QUERY_GREATER_THAN, QUERY_LESS_THAN, ELASTIC_QUERY_STRING, ELASTIC_QUERY, FILE_OUTPUT_NAME, \
-    FILE_FORMAT, SCROLL_STR, FILE_OUTPUT_PATH, MAX_SIZE, ELASTIC_SCHEME, ELASTIC_URL, ELASTIC_PORT, ELASTIC_INDEX
+    FILE_FORMAT, SCROLL_STR, FILE_OUTPUT_PATH, SIZE, ELASTIC_SCHEME, ELASTIC_URL, ELASTIC_PORT, ELASTIC_INDEX, \
+    MAX_CONTENT, MAX_SIZE
 
 if __name__ == "__main__":
     try:
-        greater_than: str = QUERY_GREATER_THAN
-        less_than: str = QUERY_LESS_THAN
-        query: str = ""
+        params = ElasticParams(gte=QUERY_GREATER_THAN,
+                               lte=QUERY_LESS_THAN,
+                               query_string=ELASTIC_QUERY_STRING,
+                               query=ELASTIC_QUERY,
+                               size=SIZE,
+                               max_size=MAX_SIZE,
+                               output_path=FILE_OUTPUT_PATH,
+                               scroll=SCROLL_STR,
+                               max_content=MAX_CONTENT)
         es: Elasticsearch = None
-        query_string: str = ELASTIC_QUERY_STRING
-        query: str = ELASTIC_QUERY
         query_type: str = ''
         file_output_name: str = FILE_OUTPUT_NAME
         file_format: str = FILE_FORMAT
-        scroll_str: str = SCROLL_STR
-        file: str = ''
         output_path: str = FILE_OUTPUT_PATH
-        headers: bool = False
-        showquery: bool = False
-        max_size: int = MAX_SIZE
         elastic_scheme: str = ELASTIC_SCHEME
         elastic_url: str = ELASTIC_URL
         elastic_port: int = ELASTIC_PORT
         elastic_index: str = ELASTIC_INDEX
-        rawquery: str = ''
 
         opts, args = getopt.getopt(sys.argv[1:],
                                    "hu:p:i:g:l:qs:q:t:o:f:s:d:c:show:m:r:",
@@ -48,7 +48,7 @@ if __name__ == "__main__":
                                     "output_path=",
                                     "columns=",
                                     "showquery=",
-                                    "max_size=",
+                                    "size=",
                                     "rawquery="
                                     ])
 
@@ -63,40 +63,44 @@ if __name__ == "__main__":
             if opt == "-u" or opt == "--url" or opt == "-host" or opt == "--host":
                 elastic_url = arg
             if opt == "--query_string":
-                query_string = arg
+                params.query_string = arg
             if opt == "-q" or opt == "--query":
-                query = arg
+                params.query = arg
             if opt == "-t" or opt == "--type":
                 query_type = arg
             if opt == "-o" or opt == "--output":
                 file_output_name = arg
             if opt == "-f" or opt == "--format":
                 file_format = arg
-            if opt == "-g" or opt == "--greater_than":
-                greater_than = arg
-            if opt == "-l" or opt == "--less_than":
-                less_than = arg
+            if opt == "-gt" or opt == "--greater_than":
+                params.gte = arg
+            if opt == "-lt" or opt == "--less_than":
+                params.lte = arg
             if opt == "--scroll":
-                scroll_str = arg
-            if opt == "-m" or opt == "--max_size":
-                max_size = int(arg)
+                params.scroll = arg
+            if opt == "-l" or opt == "--limit":
+                params.limit = int(arg)
             if opt == "-d" or opt == "--output_path":
-                output_path = arg
+                params.output_path = arg
             if opt == "-c" or opt == "--columns":
-                headers = arg.lower() in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh']
+                params.headers = arg.lower() in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh']
             if opt == "--showquery":
-                showquery = arg.lower() in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh']
+                params.showquery = arg.lower() in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh']
             if opt == "--rawquery":
-                rawquery = arg
+                params.rawquery = arg
+            if opt == "--max_size":
+                params.max_size = arg
+            if opt == "--max_content":
+                params.max_content = arg
 
         es = connect([f"{elastic_scheme}://{elastic_url}:{elastic_port}"])
         logger.debug(
-            f"Parameters {query_string}:{query}:{query_type}:{file_output_name}:{file_format}:{scroll_str}:{output_path}:{headers}:{showquery}")
+            f"Parameters {params.query_string}:{params.query}:{file_output_name}:{file_format}:{params.scroll}:{output_path}:{params.headers}:{params.showquery}")
 
         if es is None:
             raise Exception("Elasticsearch not connected")
 
-        file = create_if_not_exist(f'{file_output_name}.{file_format}', output_path)
+        params.file = create_if_not_exist(f'{file_output_name}.{file_format}', output_path)
 
         if es is not None:
             logger.debug(es)
@@ -105,20 +109,10 @@ if __name__ == "__main__":
                     res = info(es)
                     logger.debug(f"{res}" + "\n")
             else:
+                logger.debug(f"{params.gte}")
                 res = search(es,
                              elastic_index,
-                             greater_than,
-                             less_than,
-                             max_size,
-                             query,
-                             query_string,
-                             scroll_str,
-                             None,
-                             file,
-                             headers,
-                             None,
-                             showquery,
-                             rawquery)
+                             params)
                 logger.debug(f"{res}" + "\n")
         sys.exit(0)
     except KeyboardInterrupt:
